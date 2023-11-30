e1439dec7f63d348e9290c329b140edd83c97a2b
Naveenkumar1198 <naveenksb1198@gmail.com>
2023-11-30 12:52:20 +0530

    Browser commit

---
+++ src/test/java/com/DemoDec/dec1/Sample5Test.java
@@ -0,0 +1,17 @@
+package com.DemoDec.dec1;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class Sample5Test {
+	
+	
+	@Test
+	public void m1Test()
+	{
+		WebDriver driver=new ChromeDriver();
+		driver.get("https://www.amazon.in");
+	}
+
+}
